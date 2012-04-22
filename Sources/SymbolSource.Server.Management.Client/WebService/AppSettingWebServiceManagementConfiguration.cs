2083134b0b544c0415f11249b0981ab4a1a27028
Kamil Serwus <kserwus@gmail.com>
2012-04-22 21:01:17 +0200

    Added AppSetting configuration class for WebService

---
+++ Sources/SymbolSource.Server.Management.Client/WebService/AppSettingWebServiceManagementConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace SymbolSource.Server.Management.Client
+{
+    public class AppSettingWebServiceManagementConfiguration : IWebServiceManagementConfiguration
+    {
+        public string ManagementProxyPath
+        {
+            get { return ConfigurationManager.AppSettings["ManagementProxyPath"]; }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
