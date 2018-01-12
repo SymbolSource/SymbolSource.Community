20fc819dac12d551d2691f23841a909c410d2b8d
Alexander Powolozki <alexander.powolozki@de.rhenus.com>
2018-01-12 10:56:37 +0100

    - openwrap integration removed - system.web.mvc update to version 4.0

--- Sources/SymbolSource.Server.Basic/MicroKernel.cs
+++ Sources/SymbolSource.Server.Basic/MicroKernel.cs
@@ -5,7 +5,6 @@ using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.Windsor;
 using SymbolSource.Gateway.Core;
 using SymbolSource.Gateway.NuGet.Core;
-using SymbolSource.Gateway.OpenWrap.Core;
 using SymbolSource.Gateway.WinDbg.Core;
 using SymbolSource.Processing.Basic;
 
@@ -30,12 +29,6 @@ namespace SymbolSource.Server.Basic
 
             container.Register(
                 AllTypes.FromAssembly(typeof(Gateway.NuGet.Core.AttributeRouting).Assembly)
-                    .BasedOn<IController>()
-                    .LifestyleTransient()
-                );
-
-            container.Register(
-                AllTypes.FromAssembly(typeof(Gateway.OpenWrap.Core.AttributeRouting).Assembly)
                     .BasedOn<IController>()
                     .LifestyleTransient()
                 );
@@ -77,12 +70,6 @@ namespace SymbolSource.Server.Basic
                 .Register(
                     Component.For<INuGetGatewayVersionExtractor, IGatewayVersionExtractor>()
                         .ImplementedBy<NuGetGatewayVersionExtractor>()
-                        .OnlyNewServices()
-                    )
-    
-                .Register(
-                    Component.For<IOpenWrapGatewayManager>()
-                        .ImplementedBy<OpenWrapGatewayManager>()
                         .OnlyNewServices()
                     )
     
